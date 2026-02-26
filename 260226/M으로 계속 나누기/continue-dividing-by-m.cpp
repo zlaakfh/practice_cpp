50b3dcdfbd94f23a736957948203c7ad6bb302d9
chan <162523033+zlaakfh@users.noreply.github.com>
2026-02-26 15:13:27 +0900

    Title: M으로 계속 나누기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260226/M으로 계속 나누기/continue-dividing-by-m.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N, M;
+
+    cin >> N;
+    cin >> M;
+    
+    // Please write your code here.
+    while (N > 0) {
+        cout << N << "\n";
+        N /= M;
+    }
+    return 0;
+}
