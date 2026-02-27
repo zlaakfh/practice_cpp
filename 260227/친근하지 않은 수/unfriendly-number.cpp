ab86804cd8d316c606bff621458ab2e187c0d118
chan <162523033+zlaakfh@users.noreply.github.com>
2026-02-27 15:31:03 +0900

    Title: 친근하지 않은 수, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260227/친근하지 않은 수/unfriendly-number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    int cnt = 0;
+    for (int i = 0; i < n; i++) {
+        if (!(i % 2 == 0 || i % 3 == 0 || i % 5 == 0)) {
+            cnt++;
+        }
+    }
+    cout << cnt;
+    return 0;
+}
