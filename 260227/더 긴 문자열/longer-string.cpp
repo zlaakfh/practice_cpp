f51a59f48f2a1a094b5e10f1c1133a1a1849b493
chan <162523033+zlaakfh@users.noreply.github.com>
2026-02-27 14:54:54 +0900

    Title: 더 긴 문자열, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260227/더 긴 문자열/longer-string.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string a, b;
+    cin >> a >> b;
+
+    if (a.length() > b.length()) {
+        cout << a << " " << a.length();
+    }
+    else if (a.length() < b.length()) {
+        cout << b << " " <<  b.length();
+    }
+    else {
+        cout << "same";
+    }
+    return 0;
+}
