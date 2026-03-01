29e9138410f3deb085a39c4b8f55c2e2347db025
chan <162523033+zlaakfh@users.noreply.github.com>
2026-03-01 15:29:25 +0900

    Title: 특정 원소들의 합, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260301/특정 원소들의 합/sum-of-specific-elements.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int arr[4][4];
+    int total = 0;
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            cin >> arr[i][j];
+            if (i >= j) {
+                total += arr[i][j];
+            }
+        }
+    }
+    cout << total;
+    return 0;
+}
