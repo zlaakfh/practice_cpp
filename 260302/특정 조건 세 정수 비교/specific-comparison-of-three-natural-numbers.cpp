a59ac3b373b8903b99a57a5dfa978cc7e87a3306
chan <162523033+zlaakfh@users.noreply.github.com>
2026-03-02 15:31:20 +0900

    Title: 특정 조건 세 정수 비교, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260302/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if (a <= b && a <= c) {
+        cout << 1 << " ";
+    }
+    else {
+        cout << 0 << " ";
+    }
+
+    if ( a == b && b == c) {
+        cout << 1;
+    }
+    else {
+        cout << 0;
+    }
+    return 0;
+}
