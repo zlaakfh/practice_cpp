b4ea723ae4c946d4e3be532d0e2122ed1f6103d0
chan <162523033+zlaakfh@users.noreply.github.com>
2026-03-05 16:55:54 +0900

    Title: 구구단 만들기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260305/구구단 만들기/print-multiplication-table.cpp
+++ 260305/구구단 만들기/print-multiplication-table.cpp
@@ -7,14 +7,10 @@ int main() {
     cin >> a >> b;
 
     for (int i = 1; i < 10; i++) {
-        int cnt = b - a;
-        for (int j = b; j >= a; j--) {
-            if (j % 2 == 0) {
-                cout << j << " * " << i << " = " << j*i;
-                cnt -= 2;
-                if (cnt >= 0) {
-                    cout << " / ";
-                }
+        for (int j = b; j >= a; j-=2) {
+            cout << j << " * " << i << " = " << j*i;
+            if (j != a) {
+                cout << " / ";
             }
         }
         cout << endl;
