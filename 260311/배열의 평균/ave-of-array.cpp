f303e34e91f65c5dcef727c5b88bff0af1747732
chan <162523033+zlaakfh@users.noreply.github.com>
2026-03-11 15:23:31 +0900

    Title: 배열의 평균, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260311/배열의 평균/ave-of-array.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int arr[2][4];
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 4; j++) {
+            cin >> arr[i][j];
+        }
+        cout << endl;
+    }
+
+    int row_avg1 = 0;
+    int row_avg2 = 0;
+    int col_avg1 = 0;
+    int col_avg1 = 0;
+    int col_avg1 = 0;
+    return 0;
+}
