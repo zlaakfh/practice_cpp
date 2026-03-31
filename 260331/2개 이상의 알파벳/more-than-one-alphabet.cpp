74c36cdbb008d7bd9cf41ca19f6502ccdf33e7e3
chan <162523033+zlaakfh@users.noreply.github.com>
2026-03-31 09:28:01 +0900

    Title: 2개 이상의 알파벳, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260331/2개 이상의 알파벳/more-than-one-alphabet.cpp
+++ 260331/2개 이상의 알파벳/more-than-one-alphabet.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 bool IsSame(string s) {
-    int cnt = 0;
+    int cnt = 1;
     for (int i = 1; i < s.length(); i++) {
         if (s[i] != s[i-1]) cnt++;
         if (cnt >= 2) return true;
