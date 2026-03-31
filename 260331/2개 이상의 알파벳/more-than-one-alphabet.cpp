1b4818c911ae686d101b953d4025c8ff828d91e9
chan <162523033+zlaakfh@users.noreply.github.com>
2026-03-31 09:20:12 +0900

    Title: 2개 이상의 알파벳, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260331/2개 이상의 알파벳/more-than-one-alphabet.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool IsSame(string s) {
+    int cnt = 0;
+    for (int i = 1; i < s.length(); i++) {
+        if (s[i] != s[i-1]) cnt++;
+        if (cnt >= 2) return true;
+    }
+    return false;
+}
+
+int main() {
+    string s;
+    cin >> s;
+
+    if (IsSame(s)) cout << "Yes";
+    else cout << "No";
+
+    return 0;
+}
