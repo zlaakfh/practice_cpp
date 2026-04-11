292bb0d2ac6a26ab98a662c837bd3068a414157b
chan <162523033+zlaakfh@users.noreply.github.com>
2026-04-11 23:01:02 +0900

    Title: 개인정보, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260411/개인정보/personal-info.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+class Person {
+    public:
+        string name;
+        int h;
+        double w;
+};
+
+int main() {
+    Person p[5];
+
+    for (int i = 0; i < 5; i++)   {
+        cin >> p[i].name >> p[i].h >> p[i].w;
+    }
+
+    sort(p, p + 5, [](Person a, Person b) {
+        return a.name < b.name;
+    });
+
+    cout << "name" << endl;
+    for (int i = 0; i < 5; i++) {
+        cout << p[i].name << " " << p[i].h <<  " " << p[i].w << endl;
+    }
+
+    sort(p, p + 5, [](Person a, Person b) {
+        return a.h > b.h;
+    });
+    
+    cout << endl;
+    cout << "height" << endl;
+    for (int i = 0; i < 5; i++) {
+        cout << p[i].name << " " << p[i].h <<  " " << p[i].w << endl;
+    }
+
+    return 0;
+}
