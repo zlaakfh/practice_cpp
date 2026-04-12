a4031087fcd2bf49d85da13ddc3f72eca1918e4b
chan <162523033+zlaakfh@users.noreply.github.com>
2026-04-12 16:18:53 +0900

    Title: 줄 세우기 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260412/줄 세우기 2/line-up-students-2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class Student {
+    public:
+        int h, w, num;
+};
+
+int main() {
+    int n;
+    cin >> n;
+
+    Student s[n];
+    for (int i = 0; i < n; i++) {
+        cin >> s[i].h >> s[i].w;
+        s[i].num = i + 1;
+    }
+
+    sort(s, s + n, [](Student a, Student b) {
+        if (a.h != b.h) return a.h < b.h;
+        return a.w > b.w;
+    });
+
+    for (int i = 0; i < n; i++) {
+        cout << s[i].h << " " << s[i].w << " " << s[i].num << endl;
+    }
+
+    return 0;
+}
