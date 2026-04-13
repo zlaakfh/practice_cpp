7f9201410b3153c8650128a21b99a5adb58a5ac9
chan <162523033+zlaakfh@users.noreply.github.com>
2026-04-13 09:11:15 +0900

    Title: DateTime to DateTime, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260413/DateTime to DateTime/datetime-to-datetime.cpp
+++ 260413/DateTime to DateTime/datetime-to-datetime.cpp
@@ -9,31 +9,10 @@ int main() {
     int d2, h2, m2;
     cin >> d2 >> h2 >> m2;
 
-    int time = 0;
+    int start = d1 * 24 * 60 + h1 * 60 + m1;
+    int end = d2 * 24 * 60 + h2 * 60 + m2;
 
-    if (d2 < d1) time = -1;
-    else if (d2 == d1 && h2 < h1) time = -1;
-    else if (d2 == d1 && h2 == h1 && m2 < m1) time = -1;
-
-    else {
-        while (true) {
-            if (d1 == d2 && h1 == h2 && m1 == m2) break;
-
-            m1++;
-            time++;
-
-            if (m1 == 60) {
-                m1 = 0;
-                h1++;
-            }
-
-            if (h1 == 24) {
-                h1 = 0;
-                d1++;
-            }
-        }
-    }
-    
-    cout << time;
+    if (end < start) cout << -1;
+    else cout << end - start;
     return 0;
 }
